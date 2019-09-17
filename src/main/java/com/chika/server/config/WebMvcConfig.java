333b2c78f5c36c848e5843ff8b8554f3eeaa5e15
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-09-17 17:14:48 +0700

    make it better

--- src/main/java/com/chika/server/config/WebMvcConfig.java
+++ src/main/java/com/chika/server/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.chika.server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,9 +12,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @since 16-08-2019
  */
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final long MAX_AGE_SECS = 3600;
+    private final long MAX_AGE_SECS = 4000;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
