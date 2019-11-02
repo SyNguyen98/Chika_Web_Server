779f082eb8b824e61203826aab94686802dc4bcd
Sy Nguyen <48832411+SyNguyen98@users.noreply.github.com>
2019-11-02 14:37:48 +0700

    update auth response

--- src/main/java/com/chika/server/payload/responses/AuthenticationResponse.java
+++ src/main/java/com/chika/server/payload/responses/AuthenticationResponse.java
@@ -1,17 +1,23 @@
 package com.chika.server.payload.responses;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author Sy Nguyen
  * @version 1.0
- * @since 16-8-2019
+ * @since 02-11-2019
  */
 @Data
 public class AuthenticationResponse {
 
     private String tokenType = "Bearer";
     private String accessToken;
+
+    @Value("${app.mqttUsername}")
+    private String mqttUsername;
+    @Value("${app.mqttPassword}")
+    private String mqttPassword;
 
     public AuthenticationResponse(String accessToken) {
         this.accessToken = accessToken;
