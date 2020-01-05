eca6be3e7bb3a9545e98815e32c95778b9469e17
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2020-01-06 01:25:58 +0700

    update api interface

--- src/main/java/com/chika/server/controllers/ApiController.java
+++ src/main/java/com/chika/server/controllers/ApiController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApiController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home() {
-        return "ChikaApi";
+        return "Chika Api";
     }
 }
