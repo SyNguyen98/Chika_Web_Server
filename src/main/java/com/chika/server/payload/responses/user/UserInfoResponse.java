84913e5c1e213727d1df6defa3646d46ed847a01
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2020-03-05 23:46:20 +0700

    add create at for user

--- src/main/java/com/chika/server/payload/responses/user/UserInfoResponse.java
+++ src/main/java/com/chika/server/payload/responses/user/UserInfoResponse.java
@@ -1,5 +1,6 @@
 package com.chika.server.payload.responses.user;
 
+import com.chika.server.Formatter;
 import com.chika.server.models.account.User;
 import com.chika.server.models.user.UserInfo;
 import com.chika.server.services.RoleService;
@@ -8,6 +9,7 @@ import lombok.Data;
 @Data
 public class UserInfoResponse {
 
+    private String createAt;
     private String avatar;
     private String name;
     private String birthday;
@@ -17,6 +19,7 @@ public class UserInfoResponse {
     private String role;
 
     public UserInfoResponse(User user, UserInfo userInfo) {
+        this.createAt = Formatter.formatTimeDay(user.getCreatedAt());
         this.avatar = userInfo.getAvatar();
         this.name = user.getName();
         this.phone = user.getPhone();
